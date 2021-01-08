6f7c8c4eab6f617818d91bd83000c223909aa44b
MMcClure11 <mcclme02@luther.edu>
2021-01-08 14:21:41 -0600

    Refactor App.js to class component so can use componentDidMount to test fetch to giphy api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       i am your app
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+  componentDidMount(){
+    // fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=COI788NHznUTLid1ciKLW2cPSQ4voDn1`)
+    fetch(`https://api.giphy.com/v1/gifs/search?q=cats&api_key=COI788NHznUTLid1ciKLW2cPSQ4voDn1`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+
+  //data I will want for each gif:
+  // <img src={giph.images.original.url} alt={giph.title} />
+
+  render() {
+    return (
+      <div className="App">
+        I am the APP
+      </div>
+    )
+  }
 }
 
-export default App;
