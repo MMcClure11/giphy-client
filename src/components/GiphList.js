9d6c872838157fd0288f2fa3e15976fe9fd81267
MMcClure11 <mcclme02@luther.edu>
2021-01-08 14:33:56 -0600

    Add GiphList component to GiphListContainer

---
+++ src/components/GiphList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GiphList = () => {
+  return (
+    <div>
+      This is the GiphList
+    </div>
+  )
+}
+
+export default GiphList
--- src/containers/GiphListContainer.js
+++ src/containers/GiphListContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import GiphList from '../components/GiphList'
 
 const GiphListContainer = () => {
   return (
     <div>
       This is the GiphListContainer
+      <GiphList />
     </div>
   )
 }
