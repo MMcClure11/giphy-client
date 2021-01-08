d09654a65a8937fbdd81502afb43e02d11d772d6
MMcClure11 <mcclme02@luther.edu>
2021-01-08 15:12:43 -0600

    Add Gif title and image to GiphCard

--- src/components/GiphCard.js
+++ src/components/GiphCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const GiphCard = () => {
+const GiphCard = (props) => {
+  
   return (
     <div>
-      I am a GiphCard
+      <h3>{ props.giph.title }</h3>
+      <img src={ props.giph.images.original.url} alt={ props.giph.title} />
     </div>
   )
 }
