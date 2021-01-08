83c7e0fc255385cc5032283f599c7c96f0b57034
MMcClure11 <mcclme02@luther.edu>
2021-01-08 14:35:18 -0600

    Add GiphSearch component to GiphListContainer

---
+++ src/components/GiphSearch.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GiphSearch = () => {
+  return (
+    <div>
+      This is the GiphSearch
+    </div>
+  )
+}
+
+export default GiphSearch
--- src/containers/GiphListContainer.js
+++ src/containers/GiphListContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import GiphList from '../components/GiphList'
+import GiphSearch from '../components/GiphSearch'
 
 const GiphListContainer = () => {
   return (
     <div>
       This is the GiphListContainer
+      <GiphSearch />
       <GiphList />
     </div>
   )
