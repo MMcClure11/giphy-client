8e663237ee4117c7cb3dfa233b62a72cad72a9c0
MMcClure11 <mcclme02@luther.edu>
2021-01-10 11:36:48 -0600

    Add username form to favourite a giph

--- src/components/GiphCard.js
+++ src/components/GiphCard.js
@@ -3,10 +3,15 @@ import React from 'react'
 const GiphCard = (props) => {
   
   return (
-    <div>
+    <>
       <h3>{ props.giph.title }</h3>
       <img src={ props.giph.images.original.url} alt={ props.giph.title} />
-    </div>
+      <form >
+        <label>Username:</label>
+        <input name="username" type="text"   />
+        <button type="submit">Favorite</button>
+      </form>
+    </>
   )
 }
 
