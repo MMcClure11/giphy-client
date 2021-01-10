cf4a27973153c77cb9c0dff1c26d251e620a799a
MMcClure11 <mcclme02@luther.edu>
2021-01-10 11:40:37 -0600

    Add username controlled form functions

--- src/components/GiphCard.js
+++ src/components/GiphCard.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const GiphCard = (props) => {
-  
+
+  const [username, setUsername] = useState('')
+
+  const onChange = event => {
+    setUsername(event.target.value)
+  }
+  console.log(username)
   return (
     <>
       <h3>{ props.giph.title }</h3>
       <img src={ props.giph.images.original.url} alt={ props.giph.title} />
       <form >
         <label>Username:</label>
-        <input name="username" type="text"   />
+        <input name="username" type="text" onChange={ onChange } value={ username }  />
         <button type="submit">Favorite</button>
       </form>
     </>
