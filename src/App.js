d1f4f7b19176b0736b3c6b96b542e92c35e8cf96
MMcClure11 <mcclme02@luther.edu>
2021-01-10 11:33:25 -0600

    Remove commented out code

--- src/App.js
+++ src/App.js
@@ -10,27 +10,3 @@ function App() {
 }
 
 export default App;
-
-// import React, { Component } from 'react'
-
-// export default class App extends Component {
-
-//   componentDidMount(){
-    // fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=COI788NHznUTLid1ciKLW2cPSQ4voDn1`)
-    // // fetch(`https://api.giphy.com/v1/gifs/search?q=cats&api_key=COI788NHznUTLid1ciKLW2cPSQ4voDn1`)
-    // .then(res => res.json())
-    // .then(data => console.log(data))
-//   }
-
-//   //data I will want for each gif:
-//   // <img src={giph.images.original.url} alt={giph.title} />
-
-//   render() {
-//     return (
-//       <div className="App">
-//         I am the APP
-//       </div>
-//     )
-//   }
-// }
-
