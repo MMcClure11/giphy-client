cf2e665cac249a8d009810479f6a5928e8419263
MMcClure11 <mcclme02@luther.edu>
2021-01-10 12:16:23 -0600

    Add BrowserRouter and makes routes for root and user favorites

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import GiphListContainer from './containers/GiphListContainer'
+import UserFavorites from './components/UserFavorites'
 
 function App() {
   return (
     <div className="App">
-      <GiphListContainer />
+      <Router>
+        <Switch>
+          <Route exact path='/users/:id' component={ UserFavorites } />
+          <Route exact path='/' component={ GiphListContainer }/>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/UserFavorites.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserFavorites() {
+  return (
+    <div>
+      I am a particular user's list of favorited gifs
+    </div>
+  )
+}
+
+export default UserFavorites
